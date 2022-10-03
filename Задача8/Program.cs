0de7f5fd08df3b53bc2db7662bb67cf59fa7d827
Jane <lavr0892@yandex.ru>
2022-10-03 12:46:11 +0300

    Исправила sum

--- Задача8/Program.cs
+++ Задача8/Program.cs
@@ -1,8 +1,9 @@
 ﻿Console.WriteLine("Введите число: ");
 int max = Convert.ToInt32(Console.ReadLine());
-int sum = 1;
+int sum = 0;
 for (int i = 0; i <= max; i += 2)
     {
         Console.WriteLine(i);
         sum += i;
     }
+
