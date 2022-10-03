123bb0781f44adf592058934cd40d85ff0f7edf6
Jane <lavr0892@yandex.ru>
2022-10-03 10:15:54 +0300

    Add Задача2

---
+++ Задача2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два числа: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+if (n > n1)
+Console.Write(n);
+    
+else
+Console.Write(n1);
+
+Console.WriteLine(" Большее");
