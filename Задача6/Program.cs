740b97b4376fd2e1612a3b6e18d352d47d8ec09f
Jane <lavr0892@yandex.ru>
2022-10-03 10:59:40 +0300

    Задача6 решена

---
+++ Задача6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0)
+
+Console.Write("Число четное");
+
+else
+
+Console.Write("Число нечетное");
+
+
