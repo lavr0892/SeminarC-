5094282c545dac806955a96878fbd80f53a35c76
Jane <lavr0892@yandex.ru>
2022-10-04 15:32:09 +0300

    Add Задача10

---
+++ Задача10/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Вариант 1
+//Console.Clear();
+//Console.WriteLine("Введите трехзначное число ");
+//int n = Convert.ToInt32(Console.ReadLine());
+//int num2 = n % 100 /10;
+//
+//
+//Console.WriteLine(num2);
+
+
+// Вариант 2
+Console.Clear();
+Console.WriteLine("Введите трехзначное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Второе число {n % 100 / 10}");
