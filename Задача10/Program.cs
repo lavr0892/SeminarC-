8823e86f96a13e471886ae42ffe7c7225f7c4305
Jane <lavr0892@yandex.ru>
2022-10-05 14:43:24 +0300

    Перезапись

--- Задача10/Program.cs
+++ Задача10/Program.cs
@@ -1,21 +1,22 @@
 ﻿
-// Вариант 1
-//Console.Clear();
-//Console.WriteLine("Введите трехзначное число ");
-//int n = Convert.ToInt32(Console.ReadLine());
-//int num2 = n % 100 /10;
-//
-//
-//Console.WriteLine(num2);
-
 
 // Вариант 2
+/*
 Console.Clear();
 Console.WriteLine("Введите трехзначное число ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-if(n > 999 && n <= 99)
-Console.WriteLine("Введено не трехзначное число");
-
 Console.WriteLine($"Второе число {n % 100 / 10}");
 
+*/
+Console.Clear();
+Console.WriteLine("Введите трехзначное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >=100 && n <=999)
+    n = n % 100 / 10;
+
+Console.WriteLine(n);
+
+//if (n <= 99 && n > 999)
+//Console.WriteLine("Введено не трехзначное число");
