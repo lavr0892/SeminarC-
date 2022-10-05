fdcf97cd7f468e09942e4782df7c2ecb182fcb52
Jane <lavr0892@yandex.ru>
2022-10-05 14:20:56 +0300

    Пересохранение

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -1,11 +1,25 @@
-﻿Console.WriteLine("Введите два числа: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int n1 = Convert.ToInt32(Console.ReadLine());
+﻿//Console.WriteLine("Введите два числа: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+//int n1 = Convert.ToInt32(Console.ReadLine());
 
-if (n > n1)
-Console.Write(n);
+//if (n > n1)
+//Console.Write(n);
     
-else
-Console.Write(n1);
+//else
+//Console.Write(n1);
 
-Console.WriteLine(" Большее");
+//Console.WriteLine(" Большее");
+
+
+// task 2
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int maxi = n;
+if (maxi < m)
+    maxi = m;
+
+Console.WriteLine(maxi);
