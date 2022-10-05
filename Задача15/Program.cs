b62ba14c74ed49f8292186a9b6578797276f0ce4
Jane <lavr0892@yandex.ru>
2022-10-05 14:24:07 +0300

    Задача решена

--- Задача15/Program.cs
+++ Задача15/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число, соответствующее одному из дней недели: Понедельник: 1, Вторник: 2, Среда: 3, Четверг: 4, Пятница: 5, Суббота: 6, Воскресенье: 7 ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] week = {1, 2, 3, 4, 5, 6, 7};
+if (n > 5 && n < 8)
+  Console.WriteLine("Выходной день");
+
+if (n >= 1 && n <= 5)
+  Console.WriteLine("Рабочий день");
+  
+if (n > 7)
+  Console.WriteLine("Введено число, не входящее в диапазон от 1 до 7");
