cce4bf5664c690b33bc99ea56f7ce228d92171d4
Jane <lavr0892@yandex.ru>
2022-10-08 14:11:36 +0300

    Задача решена

---
+++ Задача19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int size = number.Length;
+
+if (size == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+      Console.WriteLine($"{number} - Палиндром");
+    
+    else
+      Console.WriteLine($"{number} - Не палиндром");
+  
+}
+else
+
+    Console.WriteLine($"{number} - не является пятизначным");
