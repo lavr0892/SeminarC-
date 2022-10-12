1aa3adf455f2159d9821220415951d0a92d5f2fb
Jane <lavr0892@yandex.ru>
2022-10-12 11:25:29 +0300

    Добавила отрицательное

--- Задача25/Program.cs
+++ Задача25/Program.cs
@@ -4,6 +4,7 @@ int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int m = Convert.ToInt32(Console.ReadLine());
 int result = n;
+n = Math.Abs(n);
 for(int i = 1; i < m; i++)
 {    
   result = result * n;
