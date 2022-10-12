553942127ccfde5591daa300d8d65f0bf626259f
Jane <lavr0892@yandex.ru>
2022-10-12 11:17:31 +0300

    Задача решена

---
+++ Задача25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int result = n;
+for(int i = 1; i < m; i++)
+{    
+  result = result * n;
+}
+Console.WriteLine(result);
