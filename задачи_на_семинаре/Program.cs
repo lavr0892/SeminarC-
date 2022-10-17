064f0242c8230d64015e76296840b0e05a14ee38
Jane <lavr0892@yandex.ru>
2022-10-17 16:27:49 +0300

    Рабочий файл

--- задачи_на_семинаре/Program.cs
+++ задачи_на_семинаре/Program.cs
@@ -1,31 +1,35 @@
-﻿Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[size];
-FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
-PrintArray(numbers);
-int count = 0;
-
-for (int z = 0; z < numbers.Length; z++)
-if (numbers[z] % 2 == 0)
-count++;
-
-Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
-
-void FillArrayRandomNumbers(int[] numbers)
+﻿void FillArrayRondomNumbers(int[] numbers)
 {
-    for(int i = 0; i < numbers.Length; i++)
+    for (int i = 0; i < numbers.Length; i++)
     {
-        numbers[i] = new Random().Next(100,1000);
+        numbers[i] = new Random().Next(1, 100);
     }
 }
+
 void PrintArray(int[] numbers)
 {
-    Console.Write("[ ");
     for(int i = 0; i < numbers.Length; i++)
     {
-        Console.Write(numbers[i] + " ");
+        //Console.Write($"[{string.Join( ", ", numbers)}]");
+       Console.Write(numbers[i] + " ");
     }
-    Console.Write("]");
-    Console.WriteLine();
 }
+
+Console.WriteLine("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[n];
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if(i % 2 != 0)
+    sum = sum + numbers[i];
+    else
+    break;    
+}
+
+FillArrayRondomNumbers(numbers);
+Console.WriteLine("Массив:");
+PrintArray(numbers);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов на нечётных позициях: {sum}");
