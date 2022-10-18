c9659f3759ce2730afb533c5b041b6f8316d85e4
Jane <lavr0892@yandex.ru>
2022-10-18 08:44:28 +0300

    Задача решена

--- Задача38/Program.cs
+++ Задача38/Program.cs
@@ -1,28 +1,4 @@
-﻿Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
-double[] numbers = new double[size];
-FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
-PrintArray(numbers);
-double min = Int32.MaxValue;
-double max = Int32.MinValue;
-
-for (int z = 0; z < numbers.Length; z++)
-{
-    if (numbers[z] > max)
-        {
-            max = numbers[z];
-        }
-    if (numbers[z] < min)
-        {
-            min = numbers[z];
-        }
-}
-
-Console.WriteLine($"всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
-Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
-
-void FillArrayRandomNumbers(double[] numbers)
+﻿void FillArrayRandomNumbers(double[] numbers)
 {
     for(int i = 0; i < numbers.Length; i++)
         {
@@ -31,11 +7,37 @@ void FillArrayRandomNumbers(double[] numbers)
 }
 void PrintArray(double[] numbers)
 {
-    Console.Write("[ ");
+    
     for(int i = 0; i < numbers.Length; i++)
         {
             Console.Write(numbers[i] + " ");
         }
-    Console.Write("]");
+    
     Console.WriteLine();
 }
+
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+}
+
+Console.WriteLine($"Максимальное значение: {max}, минимальное значение: {min}");
+Console.WriteLine($"Разница: {max - min}");
+
