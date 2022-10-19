49a2ce913e415315c85ee49ba7e6c02f96e036ff
Jane <lavr0892@yandex.ru>
2022-10-19 14:15:04 +0300

    Задача решена.Добавлен х

--- Задача43/Program.cs
+++ Задача43/Program.cs
@@ -23,7 +23,7 @@ b2 = Math.Abs(b2);
 k2 = Math.Abs(k2);
 
 if (y == y1)
-Console.WriteLine($"Точка пересечения: {y}");
+Console.WriteLine($"Координаты точки пересечения пересечения: y({y}), x({x})");
 
 else
 Console.WriteLine("Точки пересечения нет");
