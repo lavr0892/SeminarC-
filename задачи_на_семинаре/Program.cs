fcaf68be650be38d36872dfe77d6febd5e2c3adf
Jane <lavr0892@yandex.ru>
2022-10-27 16:11:58 +0300

    Рабочий файл

--- задачи_на_семинаре/Program.cs
+++ задачи_на_семинаре/Program.cs
@@ -47,6 +47,7 @@ PrintMatrix(matrix);
 */
 
 // Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+/* 
 void InputMatrix(double[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -74,3 +75,37 @@ double[,] matrix = new double[n, m];
 InputMatrix(matrix);
 Console.WriteLine("Массив:");
 PrintMatrix(matrix);
+ */
+
+ //Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-100, 101);
+
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
