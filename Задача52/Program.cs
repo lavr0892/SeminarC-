f57d86698790278b201b151b7197175748e1c412
Jane <lavr0892@yandex.ru>
2022-10-27 16:11:33 +0300

    Задача решена

---
+++ Задача52/Program.cs
@@ -0,0 +1,48 @@
+﻿
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }    
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        avarage = (avarage + matrix[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+PrintMatrix(matrix);
+
+
