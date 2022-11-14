beee28e6c9e6464a371463574cbcc53bdbaff042
Jane <lavr0892@yandex.ru>
2022-11-14 11:34:52 +0300

    Задача решена

--- Задача54/Program.cs
+++ Задача54/Program.cs
@@ -15,16 +15,16 @@ void ReleaseMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            for (int z = 0; z < matrix.GetLength(1) - 1; z++)
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
             {
-                if (matrix[i, z] < matrix[i, z + 1]) 
+                if (matrix[i, k] < matrix[i, k + 1]) 
                 {
                     int temp = 0;
-                    temp = matrix[i, z];
-                    matrix[i, z] = matrix[i, z + 1];
-                    matrix[i, z + 1] = temp;
+                    temp = matrix[i, k];
+                    matrix[i, k] = matrix[i, k + 1];
+                    matrix[i, k + 1] = temp;
                 }
             }
         }
